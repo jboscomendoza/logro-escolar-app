eabfbc7ee4f8721b28d73853da7cc75773aabf0a
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2020-12-15 00:36:37 -0600

    Lectura de datos

---
+++ leer_datos.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import re
+
+
+p18 = pd.read_spss("Planea06_2018_alumnos.sav")
+
+
+nombres = list(p18.columns)
+
+
+vars_base = ["NOM_ENT", "SERV", "MARGINC", "TAM_LOC_PRIM", "SEXO", "EDAD_ACC"]
+vars_contexto = []
+[vars_contexto.append(i) for i in nombres if re.match("PAB_", i)]
+
+
+score_mat = []
+[score_mat.append(i) for i in nombres if re.match("PV.MAT", i)]
+score_lyc = []
+[score_lyc.append(i) for i in nombres if re.match("PV.LYC", i)]
+
+
+contexto = pd.concat([p18[vars_base], p18[vars_contexto]], axis=1)
+contexto["MAT"] = p18[score_mat].mean(axis = 1)
+contexto["LYC"] = p18[score_lyc].mean(axis = 1)
+
+contexto.to_csv("p18.csv")
