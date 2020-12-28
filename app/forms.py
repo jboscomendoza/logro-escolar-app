0389494dc4df3f2b8b9ef9c43b6c6741aca146fd
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2020-12-28 00:29:10 -0600

    Forms y Templates corregidos para mostrar resultados

--- app/forms.py
+++ app/forms.py
@@ -8,15 +8,13 @@ ESCOLARIDAD = [
     'Universidad o Posgrado', 'Desconocido', 
 ]
 
-NO_SI = ["No", u"Sí"]
+NO_SI = ["No", u"Sí",]
 
-CANTIDAD = [
-    'Ninguno', '1', '2', '3', '4 o más',
-]
+CANTIDAD = ['Ninguno', '1', '2', '3', '4 o más',]
 
 
 class Atributos(FlaskForm):
-    SERV = SelectField(u"Tipo de servicio", choices=[
+    SERV = SelectField(u"Tipo de escuela a la que asiste", choices=[
         'Comunitaria', 'General Pública', 'Privada', 'Indígena'
     ])
     TAM_LOC_PRIM = SelectField(u"Tamaño de la localidad", choices=[
@@ -24,39 +22,39 @@ class Atributos(FlaskForm):
     ])
     SEXO = SelectField(u"Sexo", choices=["Mujer", "Hombre"])
     EDAD_ACC = SelectField(u"Edad", choices=[
-        '11 años o menos', '12 años', '14 años o más', '13 años'
+        '11 años o menos', '12 años', '13 años', '14 años o más',
     ])
-    PAB_4 = SelectField(u"¿Cuántos cuartos se utilizan para dormir en tu casa?", choices=[
+    PAB_4 = SelectField(u"Habitaciones en casa (además de cocina y baños)", choices=[
         '1', '2', '3', '4', u'5 o más'
     ])
-    PAB_11 = SelectField(u"¿Hasta qué nivel estudió tu papá?",
+    PAB_11 = SelectField(u"Nivel de estudios del padre",
         choices=ESCOLARIDAD)
-    PAB_12 = SelectField(u"¿Hasta qué nivel estudió tu mamá?", 
+    PAB_12 = SelectField(u"Nivel de estudio de la madre", 
         choices=ESCOLARIDAD)
-    PAB_14 = SelectField(u"¿Sabes hablar una lengua indígena?", choices=NO_SI)
-    PAB_16 = SelectField(u"¿Cuántos años fuiste a preescolar?", choices=[
-        'No fui', u'1 año o menos', u'2 años', 
+    PAB_14 = SelectField(u"Habla una lengua indígena", choices=NO_SI)
+    PAB_16 = SelectField(u"Años de asistencia a preescolar", choices=[
+        u'No asistió', u'1 año o menos', u'2 años', 
         u'3 años', u'Asistió, años desconocidos', 
     ])
-    PAB_22 = SelectField(u"¿Cuántos focos hay en tu casa (incluidas lámparas)?", choices=[
-        'Ninguno', 'De 11 a 15', 'De 6 a 10', 'De 1 a 5', 'Más de 15',
+    PAB_22 = SelectField(u"Número de focos en casa (incluidas lámparas)", choices=[
+        'Ninguno', 'De 1 a 5', 'De 6 a 10', 'De 11 a 15', 'Más de 15',
     ])
-    PAB_24 = SelectField(u"¿Tienes Luz eléctrica en tu casa?", choices=NO_SI)
-    PAB_25 = SelectField(u"¿Tienes Agua potable en tu casa?", choices=NO_SI)
-    PAB_26 = SelectField(u"¿Tienes Drenaje en tu casa?", choices=NO_SI)
-    PAB_29 = SelectField(u"¿Tienes Refrigerador en tu casa?", choices=NO_SI)
-    PAB_31 = SelectField(u"¿Tienes Estufa de gas o estufa eléctrica en tu casa?", choices=NO_SI)
-    PAB_34 = SelectField(u"¿Tienes Acceso a internet en tu casa?", choices=NO_SI)
-    PAB_35 = SelectField(u"¿Cuántas computadoras o laptops hay en tu casa?", choices=CANTIDAD)
-    PAB_37 = SelectField(u"¿Cuántos automóviles hay en tu casa?", choices=CANTIDAD)
-    PAB_38 = SelectField(u"¿Cuántos teléfonos móviles o celulares hay en tu casa?", choices=CANTIDAD)
-    PAB_39 = SelectField(u"¿Cuántas tablets hay en tu casa?", choices=CANTIDAD)
-    PAB_48 = SelectField(u"¿Cuántos libros hay en tu casa?", choices=[
+    PAB_24 = SelectField(u"Luz eléctrica", choices=NO_SI)
+    PAB_25 = SelectField(u"Agua potable", choices=NO_SI)
+    PAB_26 = SelectField(u"Drenaje", choices=NO_SI)
+    PAB_29 = SelectField(u"Refrigerador", choices=NO_SI)
+    PAB_31 = SelectField(u"Estufa de gas o eléctrica", choices=NO_SI)
+    PAB_34 = SelectField(u"Acceso a internet", choices=NO_SI)
+    PAB_35 = SelectField(u"Cantidad de computadoras o laptops en casa", choices=CANTIDAD)
+    PAB_37 = SelectField(u"Cantidad de automóviles en casa", choices=CANTIDAD)
+    PAB_38 = SelectField(u"Cantidad de teléfonos móviles o celulares en casa", choices=CANTIDAD)
+    PAB_39 = SelectField(u"Cantidad de tabletas en casa", choices=CANTIDAD)
+    PAB_48 = SelectField(u"Cantidad de libros en casa?", choices=[
         'Ninguno', 'Entre 1 y 25', 'Entre 26 y 50', 'Entre 51 y 100', 'Más de 100', 
     ])
-    PAB_52 = SelectField(u"Además de ir a la escuela, ¿asistes a alguna clase de idiomas (inglés u otro)?", choices=NO_SI)
-    PAB_53 = SelectField(u"Sin contar las clases de idiomas, ¿tomas fuera de la escuela alguna clase que te ayude en las materias (Matemáticas, Español, etcétera)?", choices=NO_SI)
-    PAB_85 = SelectField(u"¿Cuánto tiempo tardas en llegar de tu casa a la escuela?", choices=[
+    PAB_52 = SelectField(u"Asiste a clases de idiomas (inglés u otro)", choices=NO_SI)
+    PAB_53 = SelectField(u"Sin contar clases de idiomas, toma clases extra (Matemáticas, Español, etc.)", choices=NO_SI)
+    PAB_85 = SelectField(u"Tiempo de traslado de la casa a la escuela", choices=[
         'Máximo 15 minutos', 'De 16 a 30 minutos', 'De 31 minutos a una hora',
         'Entre 1 y 2 horas', 'Más de 2 horas', 
     ])
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import app
 from app.forms import Atributos
 from catboost import CatBoostRegressor
 
+
 modelo_lyc = CatBoostRegressor()
 modelo_lyc.load_model("model_lyc.cbm")
 modelo_mat = CatBoostRegressor()
@@ -16,23 +17,30 @@ def inicio():
 
     if form.validate_on_submit():
         data = request.form.to_dict()
+        for i in ["csrf_token", "submit"]:
+            data.pop(i)
         return redirect(url_for("resultado", atributos=data))
     
     return render_template("index.html", form=form)
 
-@app.route("/resultado", methods=["GET", "POST"])
-def resultado():
-    data_atr = eval(request.args["atributos"])
-    for i in ["csrf_token", "submit"]:
-            data_atr.pop(i)
-    data_atr = list(data_atr.values())
-    
+
+@app.route("/resultado/<atributos>", methods=["GET", "POST"])
+def resultado(atributos):
+    data_atr = eval(atributos)
+
+    nombres = list(data_atr.keys())
+    nombres.sort()
+    data_list = [data_atr[i] for i in nombres]
+
     resultado = {}
-    resultado["lyc"] = modelo_lyc.predict(data_atr)
-    resultado["mat"] = modelo_mat.predict(data_atr)
+    resultado["lyc"] = modelo_lyc.predict(data_list)
+    resultado["mat"] = modelo_mat.predict(data_list)
+
     return redirect(url_for("resumen", resultado=resultado))
+
 
 @app.route("/resumen/<resultado>", methods=["GET"])
 def resumen(resultado):
     resultado = eval(resultado)
-    return resultado
+
+    return render_template("resumen.html", resultado=resultado)
